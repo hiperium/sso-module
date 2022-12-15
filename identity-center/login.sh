6d50c4df63df1ea89ec455fb134177b29b1f22b9
SOLOA046 <andres.solorzano.-nd@disney.com>
2022-12-15 09:54:05 -0500

    Updating scripts files

--- identity-center/login.sh
+++ identity-center/login.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-if [ -z "$AWS_PROFILE" ]; then
-  echo ""
-  read -r -p 'Please, enter the <AWS profile> to get access: [profile default] ' aws_profile
-  if [ -z "$aws_profile" ]; then
-    AWS_PROFILE='default'
-    export AWS_PROFILE
-  else
-    AWS_PROFILE=$aws_profile
-    export AWS_PROFILE
-  fi
+echo ""
+read -r -p 'Please, enter the <AWS profile> to get access: [profile default] ' hiperium_aws_profile
+if [ -z "$hiperium_aws_profile" ]; then
+  hiperium_aws_profile='default'
 fi
 
-aws sso login --profile "$AWS_PROFILE"
+aws sso login --profile "$hiperium_aws_profile"
 
 echo ""
 echo "GETTING INFORMATION FROM SSO SESSION..."
@@ -25,14 +19,14 @@ fi
 reducedAccessToken=$(echo "$accessToken" | cut -c1-15)
 echo "Access Token: $reducedAccessToken..."
 
-accountId=$(aws configure get sso_account_id --profile "$AWS_PROFILE")
+accountId=$(aws configure get sso_account_id --profile "$hiperium_aws_profile")
 if [ -z "$accountId" ]; then
   echo "Error getting the SSO Account ID..."
   exit 1
 fi
 echo "Account ID: $accountId"
 
-roleName=$(aws configure get sso_role_name --profile "$AWS_PROFILE")
+roleName=$(aws configure get sso_role_name --profile "$hiperium_aws_profile")
 if [ -z "$roleName" ]; then
   echo "Error getting the SSO Role Name..."
   exit 1
@@ -43,7 +37,7 @@ roleCredentials=$(aws sso get-role-credentials  \
   --account-id "$accountId"                     \
   --role-name "$roleName"                       \
   --access-token "$accessToken"                 \
-  --profile "$AWS_PROFILE"                      \
+  --profile "$hiperium_aws_profile"                      \
   --output json)
 if [ -z "$roleCredentials" ]; then
   echo "Error getting the SSO Role Credentials..."
@@ -54,13 +48,13 @@ echo ""
 echo "CONFIGURING CLI CREDENTIALS..."
 
 accessKeyId=$(echo "$roleCredentials" | jq -r '.roleCredentials.accessKeyId')
-aws configure set aws_access_key_id "$accessKeyId" --profile "$AWS_PROFILE"
+aws configure set aws_access_key_id "$accessKeyId" --profile "$hiperium_aws_profile"
 
 secretAccessKey=$(echo "$roleCredentials" | jq -r '.roleCredentials.secretAccessKey')
-aws configure set aws_secret_access_key "$secretAccessKey" --profile "$AWS_PROFILE"
+aws configure set aws_secret_access_key "$secretAccessKey" --profile "$hiperium_aws_profile"
 
 sessionToken=$(echo "$roleCredentials" | jq -r '.roleCredentials.sessionToken')
-aws configure set aws_session_token "$sessionToken" --profile "$AWS_PROFILE"
+aws configure set aws_session_token "$sessionToken" --profile "$hiperium_aws_profile"
 
 echo "DONE!"
 echo ""
